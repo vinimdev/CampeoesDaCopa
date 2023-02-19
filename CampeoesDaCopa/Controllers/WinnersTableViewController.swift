ae525756a90481f2311015c872f8233a9b7b35d1
Vinicius Meira <viniciusmeirabr@hotmail.com>
2023-02-19 10:52:41 -0300

    Initial commit

---
+++ CampeoesDaCopa/Controllers/WinnersTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  WinnersTableViewController.swift
+//  CampeoesDaCopa
+//
+//  Created by Vinicius Meira on 22/01/23.
+//
+
+import UIKit
+
+class WinnersTableViewController: UITableViewController {
+    
+    var worldCups: [WorldCup] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadWorldCups()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! WorldCupViewController
+        let worldCup = worldCups[tableView.indexPathForSelectedRow!.row]
+        vc.worldCup = worldCup
+    }
+    
+    func loadWorldCups() {
+        let fileURL = Bundle.main.url(forResource: "winners.json", withExtension: nil)!
+        let jsonData = try! Data(contentsOf: fileURL)
+        do {
+            worldCups = try JSONDecoder().decode([WorldCup].self, from: jsonData)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return worldCups.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! WorldCupTableViewCell
+
+        let worldCup = worldCups[indexPath.row]
+        cell.prepare(with: worldCup)
+
+        return cell
+    }
+
+}
---
+++ CampeoesDaCopa/Controllers/WorldCupViewController.swift
@@ -0,0 +1,62 @@
+//
+//  WorldCupViewController.swift
+//  CampeoesDaCopa
+//
+//  Created by Vinicius Meira on 22/01/23.
+//
+
+import UIKit
+
+class WorldCupViewController: UIViewController {
+    
+    @IBOutlet weak var ivWinner: UIImageView!
+    @IBOutlet weak var ivVice: UIImageView!
+    @IBOutlet weak var lbScore: UILabel!
+    @IBOutlet weak var lbWinner: UILabel!
+    @IBOutlet weak var lbVice: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var worldCup: WorldCup!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "World Cup \(worldCup.year)"
+        ivWinner.image = UIImage(named: "\(worldCup.winner).png")
+        ivVice.image = UIImage(named: "\(worldCup.vice).png")
+        lbWinner.text = worldCup.winner
+        lbVice.text = worldCup.vice
+        lbScore.text = "\(worldCup.winnerScore) x \(worldCup.viceScore)"
+    }
+
+}
+
+extension WorldCupViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return worldCup.matches.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let games = worldCup.matches[section].games
+        return games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! GamesTableViewCell
+        
+        let match = worldCup.matches[indexPath.section]
+        let game = match.games[indexPath.row]
+        
+        cell.prepare(with: game)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let match = worldCup.matches[section]
+        return match.stage
+    }
+    
+}
+
+extension WorldCupViewController: UITableViewDelegate {
+    
+}
